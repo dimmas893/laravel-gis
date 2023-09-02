f4da8a376ac2440391d0e55329ab0ea5d7b980f7
sebelumcahahaya <anandadimmas1204@gmail.com>
2023-09-02 23:19:33 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<img src="gambar.jpg" width="100%">
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <style>
+        .text-center {
+            text-align: center;
+        }
+
+        #map {
+            width: '100%';
+            height: 400px;
+        }
+    </style>
+    <link rel='stylesheet' href='https://unpkg.com/leaflet@1.8.0/dist/leaflet.css' crossorigin='' />
+</head>
+
+<body>
+    <h1 class='text-center'>Laravel Leaflet Maps</h1>
+    <div id='map'></div>
+
+    <script src='https://unpkg.com/leaflet@1.8.0/dist/leaflet.js' crossorigin=''></script>
+    <script>
+        let map, markers = [];
+        /* ----------------------------- Initialize Map ----------------------------- */
+        function initMap() {
+            map = L.map('map', {
+                center: {
+                    lat: 28.626137,
+                    lng: 79.821603,
+                },
+                zoom: 15
+            });
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© OpenStreetMap'
+            }).addTo(map);
+
+            map.on('click', mapClicked);
+            initMarkers();
+        }
+        initMap();
+
+        /* --------------------------- Initialize Markers --------------------------- */
+        function initMarkers() {
+            const initialMarkers = <?php echo json_encode($initialMarkers); ?>;
+
+            for (let index = 0; index < initialMarkers.length; index++) {
+
+                const data = initialMarkers[index];
+                const marker = generateMarker(data, index);
+                marker.addTo(map).bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
+                map.panTo(data.position);
+                markers.push(marker)
+            }
+        }
+
+        function generateMarker(data, index) {
+            return L.marker(data.position, {
+                    draggable: data.draggable
+                })
+                .on('click', (event) => markerClicked(event, index))
+                .on('dragend', (event) => markerDragEnd(event, index));
+        }
+
+        /* ------------------------- Handle Map Click Event ------------------------- */
+        function mapClicked($event) {
+            console.log(map);
+            console.log($event.latlng.lat, $event.latlng.lng);
+        }
+
+        /* ------------------------ Handle Marker Click Event ----------------------- */
+        function markerClicked($event, index) {
+            console.log(map);
+            console.log($event.latlng.lat, $event.latlng.lng);
+        }
+
+        /* ----------------------- Handle Marker DragEnd Event ---------------------- */
+        function markerDragEnd($event, index) {
+            console.log(map);
+            console.log($event.target.getLatLng());
+        }
+    </script>
+</body>
+
+</html>
